29099110b5dab6db2c718cb12f0264266cd7b59f
all4nitrous <all4nitrous@gmail.com>
2024-10-25 02:55:32 -0400

    reduced redundancy and added a calculation function so you wont have to calculate the funtion each time when coding

--- calculator/__init__.py
+++ calculator/__init__.py
@@ -1,15 +1,31 @@
+#from calculator.operationd import add, subtract, multiply, divide
+
+class Calculation:
+    def __init__(self, a, b, operation):
+        self.a = a
+        self.b = b
+        self.operation  = operation
+"""Allows operations between a & b"""
+def get_result(self):
+    return self.operation(self.a, self.b)
+
+
 '''Add function'''
 def add(a,b):
-    return a + b
+    calculation = Calculation(a, b, add)
+    return calculation.get.result()
 
 '''Subtract Function'''
 def subtract(a,b):
-    return a - b
+    calculation = Calculation(a, b, subtract)
+    return calculation.get.result()
 
 '''Multiplication Function'''
 def multiply(a,b):
-    return a * b
+    calculation = Calculation(a, b, multiply)
+    return calculation.get.result()
 
 '''Division Function'''
 def divide(a,b):
-    return a / b
+    calculation = Calculation(a, b, divide)
+    return calculation.get.result()
--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -1,18 +1,18 @@
 from calculator import add, subtract, multiply, divide
 '''Initial Calculator Test'''
-from calculator import add
+from calculator import Calculator
 
 '''This section checks that the addition works'''
 
 def test_addition():
-    assert add(2,2) == 4
+    assert Calculator.add(2,2) == 4
     
     '''Checks subtraction for calculator'''
 def test_subtraction():
-        assert subtract(2,2) == 0
+        assert Calculator.subtract(2,2) == 0
         
 def test_multiplication():
-        assert multiply(5,5) == 25
+        assert Calculator.multiply(5,5) == 25
         
 def test_division():
-        assert divide(6,2)  == 3
+        assert Calculator.divide(6,2)  == 3
