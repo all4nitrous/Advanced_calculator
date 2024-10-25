ce1eb91af8cf9aea624a7aeac4d91c75dfcc1632
all4nitrous <all4nitrous@gmail.com>
2024-10-25 02:25:57 -0400

    fixed touch files

--- calculator/__init__.py
+++ calculator/__init__.py
@@ -1,2 +1,15 @@
+'''Add function'''
 def add(a,b):
     return a + b
+
+'''Subtract Function'''
+def subtract(a,b):
+    return a - b
+
+'''Multiplication Function'''
+def multiply(a,b):
+    return a * b
+
+'''Division Function'''
+def divide(a,b):
+    return a / b
--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -1,4 +1,18 @@
+from calculator import add, subtract, multiply, divide
+'''Initial Calculator Test'''
 from calculator import add
+
+'''This section checks that the addition works'''
 
 def test_addition():
     assert add(2,2) == 4
+    
+    '''Checks subtraction for calculator'''
+def test_subtraction():
+        assert subtract(2,2) == 0
+        
+def test_multiplication():
+        assert multiply(5,5) == 25
+        
+def test_division():
+        assert divide(6,2)  == 3
